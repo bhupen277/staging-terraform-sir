61f78b333eb9658722a0edced3695c79a1fe9940
jarvis <chetansomkuwar@gmail.com>
2024-03-08 13:54:39 +0530

    --- Auto Git Commit ---

--- VPC/var_data.auto.tfvars
+++ VPC/var_data.auto.tfvars
@@ -1,5 +1,3 @@
 region = "us-west-2"
 
-terraform-backend = "terraform.tfstateoregon"
-
 cidr_block = "10.0.0.0/16"
--- VPC/variable.tf
+++ VPC/variable.tf
@@ -3,11 +3,6 @@ variable "region" {
   description = "providing my refion for project"
 }
 
-variable "terraform-backend" {
-  type = string
-  description = "providing my backend bucket name"
-}
-
 variable "cidr_block" {
   type = number
   description = "providing vpc IP address"
