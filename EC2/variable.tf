fd36c78432793814bff3dfa84d91bb636b51fa5b
jarvis <chetansomkuwar@gmail.com>
2024-03-09 01:06:38 +0530

    --- Auto Git Commit ---

--- EC2/variable.tf
+++ EC2/variable.tf
@@ -1,19 +1,19 @@
-variable "region" {
-  type = string
-  description = "providing my refion for project"
-}
+# variable "region" {
+#   type = string
+#   description = "providing my refion for project"
+# }
 
-variable "ami" {
-  type = string
-  description = "providing ami id to server"
-}
+# variable "ami" {
+#   type = string
+#   description = "providing ami id to server"
+# }
 
-variable "instance_type" {
-  type = string
-  description = "providing the server size"
-}
+# variable "instance_type" {
+#   type = string
+#   description = "providing the server size"
+# }
 
-variable "key_name" {
-  type = string
-  description = "providing my server key"
-}
+# variable "key_name" {
+#   type = string
+#   description = "providing my server key"
+# }
