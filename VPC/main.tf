83ca32b4395adba09f9273ef73aed0db8b31096f
jarvis <chetansomkuwar@gmail.com>
2024-03-08 14:10:22 +0530

    --- Auto Git Commit ---

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = var.region
-    shared_config_files      = ["/Users/tf_user/.aws/conf"]
+    shared_config_files      = ["/home/ubuntu/.aws/config"]
     shared_credentials_files = ["/Users/tf_user/.aws/creds"]
     profile                  = "customprofile"
 }
