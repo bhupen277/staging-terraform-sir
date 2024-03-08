492b8e293b7cfaccd3efd62d9ea5110588fac9bd
jarvis <chetansomkuwar@gmail.com>
2024-03-08 13:54:28 +0530

    --- Auto Git Commit ---

--- VPC/main.tf
+++ VPC/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = var.terraform-backend
+    bucket = "terraform.tfstateoregon"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "terraform-staging"
