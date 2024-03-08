1f69e9c0c6497e9c109eaa61ffbbe1c7654155f5
jarvis <chetansomkuwar@gmail.com>
2024-03-08 14:09:42 +0530

    --- Auto Git Commit ---

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
     region = var.region
     shared_config_files      = ["/Users/tf_user/.aws/conf"]
-  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
-  profile                  = "customprofile"
+    shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+    profile                  = "customprofile"
 }
 
 terraform {
