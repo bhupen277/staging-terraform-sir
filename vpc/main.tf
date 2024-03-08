cf776cb0cd5c6335aae8cdac0ca37aa291e51348
jarvis <chetansomkuwar@gmail.com>
2024-03-09 00:59:05 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,15 @@
 provider "aws" {
-  region = var.region
+    region = var.region
+    shared_config_files      = ["/home/ubuntu/.aws/config"]
+    shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+    profile                  = "config"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform.tfstateoregon"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-staging"
+  }
+}
