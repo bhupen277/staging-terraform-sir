9e49633c980df883a470ebfe89c5d1ece906c7ea
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:42:03 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/vpc/variable.tf
+++ MotoGP/modules/vpc/variable.tf
@@ -1,2 +1,3 @@
 # Note: till now, we just declaring the project values here, but now we are only declaring the values here. and values will mention on variable module file
-        will just initialize the variable & will pass the value from outside the vpc directory.
+#        will just initialize the variable & will pass the value from outside the vpc directory.
+
