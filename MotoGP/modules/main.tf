f4340447e85fa442bd3284aae4d7c897d3da35e7
jarvis <chetansomkuwar@gmail.com>
2024-03-11 05:19:04 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform.tfstateoregon"
+    bucket = "staging-terraformm/"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "terraform-staging"
