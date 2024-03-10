7bc21c981500c44a513ef96016a8b0a8508163be
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:43:03 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/vpc/main.tf
+++ MotoGP/modules/vpc/main.tf
@@ -1,6 +1,6 @@
 # here we create only resources and providers are defined into the projet's module main.tf file
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var
   tags = {
     Name = "${var.project}-vpc"
     enviorment = var.enviorment  
