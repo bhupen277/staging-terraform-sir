8e1845cc9db4682a8c006f80708c7cd7fbbc4a09
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:09:09 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my.id
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
