051559dadfea5504e3c7152b0f8cac63961499cb
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:21:04 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public_subnet" {
 
 # Need internet gateway to provide the internet to my public subnet
 resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "main"
