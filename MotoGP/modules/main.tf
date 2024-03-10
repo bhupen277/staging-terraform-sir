47ae3edf5e08d5225d24f07dc1d2a32fbcf756a4
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:21:45 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "${var.project}-private-subnet"
+    Name = "${var.project}-igw-subnet"
     enviorment = var.enviorment
   }
 }
