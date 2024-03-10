c73e6260c659bf43cd01f8fbfd55e8898bbcc775
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:26:37 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -47,20 +47,4 @@ resource "aws_internet_gateway" "my_igw" {
   tags = {
     Name = "${var.project}-private-subnet"
     enviorment = var.enviorment
-  }resource "aws_default_route_table" "example" {
-  default_route_table_id = aws_vpc.example.default_route_table_id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
   }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-  }
-
-  tags = {
-    Name = "example"
-  }
-}
