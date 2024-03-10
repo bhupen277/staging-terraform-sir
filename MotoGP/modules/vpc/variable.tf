16c7459d40e953a8c2c650147cfb0ac216cd0cd9
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:36:31 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/vpc/variable.tf
+++ MotoGP/modules/vpc/variable.tf
@@ -1,1 +1,1 @@
-# Note: till now, we just declaring the project values here, but now we are 
+# Note: till now, we just declaring the project values here, but now we are only declaring the values here
