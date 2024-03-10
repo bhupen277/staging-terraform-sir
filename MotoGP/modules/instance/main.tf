a90f3756dac213688bc609b1c030675608f66ab0
jarvis <chetansomkuwar@gmail.com>
2024-03-11 04:55:26 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/instance/main.tf
+++ MotoGP/modules/instance/main.tf
@@ -1,13 +1,5 @@
 resource "aws_instance" "foo" {
   ami           = var.image_id # us-west-2
   instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
+se
 }
