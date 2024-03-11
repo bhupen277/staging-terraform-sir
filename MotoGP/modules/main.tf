b75a141af458f3e38839a238f7d8f7767b1d29ba
jarvis <chetansomkuwar@gmail.com>
2024-03-11 13:08:27 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "my_sg" {
     cidr_block = ["0.0.0.0/0"]
   }
 
+  ingress = {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_block = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "allow_tls"
   }
