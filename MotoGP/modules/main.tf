f3a7720d7260107355f3b803b54dc6b13a9dce04
jarvis <chetansomkuwar@gmail.com>
2024-03-11 13:20:31 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -62,10 +62,6 @@ resource "aws_security_group" "my_sg" {
   depends_on = [ 
     module.my_vpc_module
    ]
-
-  tags = {
-    Name = "allow_tls"
-  }
 }
 
 
@@ -80,7 +76,7 @@ module "my_instance" {
   project = var.project
   enviorment = var.enviorment
   subnet_id = module.my_vpc_module.public_subnet_id  # it will create from vpc when vpc will create & this id will only bounded with vpc directory if we want sg_id from vpc so we need to use the "output_block"
-  sg_ids = 
+  sg_ids = []
 }
 
 # # Instance creating in private subnet by calling o/p
