611674a76171b114771daab0cb81d8fedfe97b50
jarvis <chetansomkuwar@gmail.com>
2024-03-11 13:08:37 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "my_sg" {
 
   ingress = {
     protocol = "tcp"
+    from_port = 443
+    to_port = 443
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+    ingress = {
+    protocol = "tcp"
     from_port = 80
     to_port = 80
     cidr_block = ["0.0.0.0/0"]
