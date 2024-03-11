0ea8de2b077cd93d89ccb793f4d7a4a8cf5a8887
jarvis <chetansomkuwar@gmail.com>
2024-03-11 12:45:10 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/main.tf
+++ MotoGP/modules/main.tf
@@ -37,3 +37,16 @@ module "my_instance" {
   subnet_id = module.my_vpc_module.public_subnet_id  # it will create from vpc when vpc will create & this id will only bounded with vpc directory if we want sg_id from vpc so we need to use the "output_block"
   sg_ids = 
 }
+
+# Instance creating in private subnet by calling o/p
+module "my_instance" {
+  source = "./modules/instance"
+  count = var.count
+  ami = var.image_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  project = var.project
+  enviorment = var.enviorment
+  subnet_id = module.my_vpc_module.public_subnet_id  # it will create from vpc when vpc will create & this id will only bounded with vpc directory if we want sg_id from vpc so we need to use the "output_block"
+  sg_ids = 
+}
