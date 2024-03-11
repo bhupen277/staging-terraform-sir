8bb63905dbec125b0e793c9520eb569f72432ee1
jarvis <chetansomkuwar@gmail.com>
2024-03-11 13:12:48 +0530

    --- Auto Git Commit ---

--- MotoGP/modules/vpc/output.tf
+++ MotoGP/modules/vpc/output.tf
@@ -6,8 +6,8 @@ output "subnet_id" {
   value = "aws_subnet.public_subnet_id"
 }
 
-output "name" {
-  
+output "my_vpc_module" {
+  value = 
 }
 
 # It is not neccessory to use call the both o/p, we call it according to requirement
